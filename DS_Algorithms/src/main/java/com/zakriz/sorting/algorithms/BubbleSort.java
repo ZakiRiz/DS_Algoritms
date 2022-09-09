c7add043105ce56d79002b69a23e13600cce0c2f
ZakiRiz <30310050+ZakiRiz@users.noreply.github.com>
2022-09-10 02:30:22 +0530

    Add files via upload

---
+++ DS_Algorithms/src/main/java/com/zakriz/sorting/algorithms/BubbleSort.java
@@ -0,0 +1,31 @@
+package com.zakriz.sorting.algorithms;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {20, 35, -15, 7, 55, 1 , -22};
+
+        for(int lastSortedIndex = arr.length-1; lastSortedIndex > 0; lastSortedIndex--){
+
+            for(int i = 0; i < lastSortedIndex; i++){
+
+                if(arr[i] >  arr[i+1]){
+                    swap(arr, i , i+1);
+                }
+            }
+        }
+        for(int j = 0; j< arr.length; j++){
+            System.out.println(arr[j]);
+        }
+    }
+
+    public static void swap(int[] arr, int i , int j){
+
+        if(i == j)return;
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
